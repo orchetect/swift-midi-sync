5ac89cfe65c6432edeb824e6f72f67fde9d2e138
Steffan Andrews <stef@orchetect.com>
2026-04-20 18:39:36 -0700

    Updated dependencies to main branch

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../swift-midi-core"), // .package(url: "https://github.com/orchetect/swift-midi-core", from: "0.1.0"),
-        .package(path: "../swift-midi-io"), // .package(url: "https://github.com/orchetect/swift-midi-io", from: "0.1.0"),
+        .package(url: "https://github.com/orchetect/swift-midi-core", branch: "main"), // TODO: from: "0.1.0"),
+        .package(url: "https://github.com/orchetect/swift-midi-io", branch: "main"), // TODO: from: "0.1.0"),
         .package(url: "https://github.com/orchetect/swift-timecode", from: "3.1.0")
     ],
     targets: [
