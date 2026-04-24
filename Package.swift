206b4c5c1e49c7dacce33d382e267a3dfcf1afef
Steffan Andrews <stef@orchetect.com>
2026-04-24 16:51:16 -0700

    Updated dependency versions

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/orchetect/swift-midi-core", branch: "main"), // TODO: from: "0.1.0"),
+        .package(url: "https://github.com/orchetect/swift-midi-core", from: "0.0.0"),
         .package(url: "https://github.com/orchetect/swift-timecode", from: "3.1.0"),
         .package(url: "https://github.com/orchetect/swift-testing-extensions", from: "0.3.0")
     ],
